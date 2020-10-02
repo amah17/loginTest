ca3653f05dd4b996ec0c6495e6bc9f98354ab254
amah17 <amah17@users.noreply.github.com>
2020-10-02 00:26:47 -0600

    Added Mongoose compatability

--- app.js
+++ app.js
@@ -5,16 +5,37 @@ const app = express();
 const expressEjsLayout = require('express-ejs-layouts')
 const flash = require('connect-flash');
 const session = require('express-session');
+
+
+// DB
+const db = require('./config/keys').MongoURI;
+
 //mongoose
-mongoose.connect('mongodb://localhost/test',{useNewUrlParser: true, useUnifiedTopology : true})
+mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology : true})
 .then(() => console.log('connected,,'))
 .catch((err)=> console.log(err));
 
 //EJS
 app.set('view engine','ejs');
 app.use(expressEjsLayout);
+
 //BodyParser
 app.use(express.urlencoded({extended : false}));
+//express session
+app.use(session({
+    secret : 'secret',
+    resave : true,
+    saveUninitialized : true
+   }));
+   //use flash
+   app.use(flash());
+   app.use((req,res,next)=> {
+     res.locals.success_msg = req.flash('success_msg');
+     res.locals.error_msg = req.flash('error_msg');
+     res.locals.error  = req.flash('error');
+   next();
+   })
+
 //Routes
 app.use('/',require('./routes/index'));
 app.use('/users',require('./routes/users'));
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://aidenM:aidenM@cluster0.wtcbm.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
